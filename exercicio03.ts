1cada9cc49d5892c9ab661ed21890a70ff03e49b
get-Friday <gabi.get@hotmail.com>
2022-04-29 16:22:54 -0300

    ex 3 .ts

---
+++ exercicio03.ts
@@ -0,0 +1,13 @@
+let aluno: {
+    nome: string,
+    matricula: number,
+    ativo: boolean,
+    cadeiras: string[] 
+}
+
+aluno.nome = 'Gabriel'
+aluno.matricula = 123456
+aluno.ativo = true
+aluno.cadeiras = ['gamer', 'escritório', 'cozinha', 'praça']
+
+console.log(aluno)
